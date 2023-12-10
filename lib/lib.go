c609f2b9d3cd27fdc91c66e58ac05982efa08985
Umputun <umputun@gmail.com>
2023-12-10 13:44:01 -0600

    update lib docs

--- lib/lib.go
+++ lib/lib.go
@@ -5,8 +5,9 @@
 // The Detector is designed to be thread-safe and supports concurrent usage.
 //
 // Before using a Detector, it is necessary to load spam data using one of the Load* methods:
+//
 //   - LoadStopWords: This method loads stop-words (stop-phrases) from provided readers. The reader can
-//     return words either as one word (or phrase) per line or as a comma-separated list of words
+//     parse words either as one word (or phrase) per line or as a comma-separated list of words
 //     (phrases) enclosed in double quotes. Both formats can be mixed within the same reader.
 //     Example of a reader stream:
 //     "word1"
@@ -20,23 +21,24 @@
 //     one based on the Naive Bayes algorithm and another on Cosine Similarity.
 //
 // Additionally, Config provides configuration options:
+//
 //   - Config.MaxAllowedEmoji specifies the maximum number of emojis permissible in a message.
 //     Messages exceeding this count are marked as spam. A negative value deactivates emoji detection.
 //
 //   - Config.MinMsgLen defines the minimum message length for spam checks. Messages shorter
 //     than this threshold are ignored. A negative value or zero deactivates this check.
 //
-// - Config.FirstMessageOnly specifies whether only the first message from a given userID should
-// be checked.
+//   - Config.FirstMessageOnly specifies whether only the first message from a given userID should
+//     be checked.
 //
-// - Config.CasAPI specifies the URL of the CAS API to use for spam detection. If this is empty, the
-// detector will not use the CAS API checks.
+//   - Config.CasAPI specifies the URL of the CAS API to use for spam detection. If this is empty, the
+//     detector will not use the CAS API checks.
 //
-// - Config.HTTPClient specifies the HTTP client to use for CAS API checks. This interface is satisfied
-// by the standard library's http.Client type.
+//   - Config.HTTPClient specifies the HTTP client to use for CAS API checks. This interface is satisfied
+//     by the standard library's http.Client type.
 //
 // Other important methods are Detector.UpdateSpam and Detector.UpdateHam, which are used to update the
 // spam and ham samples on the fly. Those methods are thread-safe and can be called concurrently.
-// To call them WithSpamUpdater and WithHamUpdater methods should be used first to provide
+// To call them Detector.WithSpamUpdater and Detector.WithHamUpdater methods should be used first to provide
 // user-defined structs that implement the SampleUpdater interface.
 package lib
