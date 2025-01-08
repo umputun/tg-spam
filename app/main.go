eb5ee84d0274777c4dc9c7c937ff5bcb36279732
Umputun <umputun@gmail.com>
2025-01-08 01:05:10 -0600

    fix regression of help cli param

--- app/main.go
+++ app/main.go
@@ -160,7 +160,7 @@ func main() {
 	p := flags.NewParser(&opts, flags.PrintErrors|flags.PassDoubleDash|flags.HelpFlag)
 	p.SubcommandsOptional = true
 	if _, err := p.Parse(); err != nil {
-		if !errors.Is(err, flags.ErrHelp) {
+		if !errors.Is(err.(*flags.Error).Type, flags.ErrHelp) {
 			log.Printf("[ERROR] cli error: %v", err)
 			os.Exit(1)
 		}
