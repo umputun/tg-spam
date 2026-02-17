3bdf14bb8f9fae28158aee739eefa8b07ac0fb52
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:12:59 +0000

    use verbose completions for zsh and fish descriptions

--- completions/tg-spam.bash
+++ completions/tg-spam.bash
@@ -1,8 +1,7 @@
 # bash completion for tg-spam (generated via go-flags)
 _tg_spam() {
     local args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
-    local IFS=$'\n'
-    COMPREPLY=($(GO_FLAGS_COMPLETION=1 "${COMP_WORDS[0]}" "${args[@]}" 2>/dev/null))
+    mapfile -t COMPREPLY < <(GO_FLAGS_COMPLETION=1 "${COMP_WORDS[0]}" "${args[@]}" 2>/dev/null)
     return 0
 }
 complete -o default -F _tg_spam tg-spam
--- completions/tg-spam.fish
+++ completions/tg-spam.fish
@@ -1,2 +1,2 @@
 # fish completion for tg-spam (generated via go-flags)
-complete -c tg-spam -a '(GO_FLAGS_COMPLETION=1 tg-spam (commandline -cop) 2>/dev/null)'
+complete -c tg-spam -a '(GO_FLAGS_COMPLETION=verbose tg-spam (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
